6040bdcfa50c23c97ed97bad636a2c5608a2719e
itolstov09 <itolstov09@gmail.com>
2022-08-31 20:41:26 +0600

    первая реализация с багами. коммит для бэкапа

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,23 +3,37 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
 
 public class Main {
+    static String resultFolderName = String.format("./results/%s/", System.currentTimeMillis());
+    static String bookName;
+
+
     public static void main(String[] args) throws IOException {
 //        File directory = new File("./");
 //        System.out.println(directory.getAbsolutePath());
 
 //        считать html файл
-        File input = new File("./html/test.html");
+//        File input = new File("./html/test.html");
+        File input = new File("./html/Pro Git.html");
         Document document = Jsoup.parse(input, "UTF-8");
         // извлечь название книги
-        String bookName = extractBookName(
+        bookName = extractBookName(
             document.body().getElementsByTag("h1").text()
         );
 
         Elements divs = document.body().getElementsByTag("div");
+
+        //создаем новую директорию для изображений и заметок
+        File resultDir = new File(resultFolderName);
+        resultDir.mkdir();
+
         for (Element div : divs) {
             if (!div.hasAttr("id")) {
                 continue;
@@ -27,14 +41,59 @@ public class Main {
 
             String divId = div.id();
 
-            String note = String.format("[[%s]]\n\n", bookName);
+            StringBuilder note = new StringBuilder();
+            note.append(String.format("[[%s]]\n\n", bookName));
 
+            Element pageNumber = div.getElementsByClass("bm-page").first();
+            if (pageNumber != null) {
+                note.append(String.format("Страница: %s\n", pageNumber.text()));
+            }
 
+            Element comment = div.getElementsByClass("bm-note").first();
+            if (comment != null) {
+                note.append(String.format("Комментарий %s\n", comment.text()));
+            }
 
+            Element textDiv = div.getElementsByClass("bm-text").first();
+            if (textDiv != null) {
+                String noteText = textDiv.text();
+                if (!(noteText.equals("Bookmark") || noteText.equals("Карандаш"))) {
+                    note.append(String.format("%s", noteText));
+                }
+            }
 
+            Element imageDiv = div.getElementsByClass("bm-image").first();
+            if (imageDiv != null) {
+                String savedImageName = saveImageAndGetName(imageDiv);
+                note.append(String.format("![[%s]]", savedImageName));
+            }
+
+            //Создание файла заметки
+            Path path = Paths.get(String.format("%s%s--%s.md", resultFolderName, bookName, System.currentTimeMillis()));
+            byte[] strToBytes = note.toString().getBytes();
+            Files.write(path, strToBytes);
+
+            System.out.println();
         }
-        System.out.println();
 
+    }
+
+    private static String saveImageAndGetName(Element imageDiv) throws IOException {
+        Element img = imageDiv.select("img").first();
+        assert img != null;
+        String imageBase64string = img.attr("src");
+
+        String name = String.format("%s_%s.png", bookName, System.currentTimeMillis());
+
+        String partSeparator = ",";
+        if (imageBase64string.contains(partSeparator)) {
+            String encodedImg = imageBase64string.split(partSeparator)[1];
+            byte[] decodedImg = Base64.getDecoder().decode(encodedImg.getBytes(StandardCharsets.UTF_8));
+            Path destinationFile = Paths.get(resultFolderName, name);
+            Files.write(destinationFile, decodedImg);
+        }
+
+        return name;
     }
 
     private static String extractBookName(String headerText) {
