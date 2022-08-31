62592c4f87d5ea5f2d91a794318758ddfa7c3bfe
itolstov09 <itolstov09@gmail.com>
2022-08-31 21:27:57 +0600

    nameing и комментарии

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,30 +11,30 @@ import java.nio.file.Paths;
 import java.util.Base64;
 
 public class Main {
-    static String resultFolderName = String.format("./results/%s/", System.currentTimeMillis());
+    static String resultFolderPath = String.format("./results/%s/", System.currentTimeMillis());
     static String bookName;
 
 
     public static void main(String[] args) throws IOException {
-//        File directory = new File("./");
-//        System.out.println(directory.getAbsolutePath());
-
 //        считать html файл
 //        File input = new File("./html/test.html");
 //        File input = new File("./html/Pro Git.html");
         File input = new File("./html/sp.html");
         Document document = Jsoup.parse(input, "UTF-8");
+
         // извлечь название книги
         bookName = extractBookName(
             document.body().getElementsByTag("h1").text()
         );
 
         //создаем новую директорию для изображений и заметок
-        File resultDir = new File(resultFolderName);
+        File resultDir = new File(resultFolderPath);
         resultDir.mkdir();
 
+        // извлекаем заметки
         Elements divs = document.body().getElementsByTag("div");
         for (Element div : divs) {
+            // div без id содержит инфу по книге и прочее
             if (!div.hasAttr("id")) {
                 continue;
             }
@@ -45,6 +45,7 @@ public class Main {
             // добавляем в заметку md ссылку на книгу
             note.append(String.format("[[%s]]\n\n", bookName));
 
+            // текст заметки
             Element textDiv = div.getElementsByClass("bm-text").first();
             if (textDiv != null) {
                 String noteText = textDiv.text();
@@ -54,14 +55,17 @@ public class Main {
                 note.append(String.format("%s\n\n", noteText));
             }
 
+            // комментарий к заметке
             Element comment = div.getElementsByClass("bm-note").first();
             if (comment != null) {
                 note.append(String.format("Комментарий %s\n\n", comment.text()));
             }
 
+            // изображение
             Element imageDiv = div.getElementsByClass("bm-image").first();
             if (imageDiv != null) {
                 String savedImageName = saveImageAndGetName(imageDiv);
+                // md ссылка на изображение
                 note.append(String.format("![[%s]]\n\n", savedImageName));
             }
 
@@ -72,8 +76,7 @@ public class Main {
 
 
             //Создание файла заметки
-//            Path path = Paths.get(String.format("%s%s--%s.md", resultFolderName, bookName, System.currentTimeMillis()));
-            Path path = Paths.get(String.format("%s%s.md", resultFolderName,  System.currentTimeMillis()));
+            Path path = Paths.get(String.format("%s%s.md", resultFolderPath,  System.currentTimeMillis()));
             byte[] strToBytes = note.toString().getBytes();
             Files.write(path, strToBytes);
 
@@ -85,19 +88,21 @@ public class Main {
     private static String saveImageAndGetName(Element imageDiv) throws IOException {
         Element img = imageDiv.select("img").first();
         assert img != null;
+        // получаем изображение
         String imageBase64string = img.attr("src");
 
-        String name = String.format("%s_%s.png", bookName, System.currentTimeMillis());
+        String imageName = String.format("%s_%s.png", bookName, System.currentTimeMillis());
 
+        // сохраняем изображение
         String partSeparator = ",";
         if (imageBase64string.contains(partSeparator)) {
             String encodedImg = imageBase64string.split(partSeparator)[1];
             byte[] decodedImg = Base64.getDecoder().decode(encodedImg.getBytes(StandardCharsets.UTF_8));
-            Path destinationFile = Paths.get(resultFolderName, name);
+            Path destinationFile = Paths.get(resultFolderPath, imageName);
             Files.write(destinationFile, decodedImg);
         }
 
-        return name;
+        return imageName;
     }
 
     private static String extractBookName(String headerText) {
